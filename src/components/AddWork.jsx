7cec2aac8ab7ce7323b01f28ee1f975e983c29c0
Kevin M Babcock <kevinmbabcock@Kevins-MacBook-Air.local>
2018-03-02 15:29:08 -0800

    add form to AddWork  component

--- src/components/AddWork.jsx
+++ src/components/AddWork.jsx
@@ -3,7 +3,50 @@ import React from 'react';
 function AddWork(){
   return (
     <div>
-      <p>AddWork component has loaded</p>
+      <h5>Fill out form to add a new work</h5>
+      <form>
+        <p>Title:</p>
+        <input
+          type='text'
+          id='title'
+          placeholder='Title'
+          ref={(input) => {_title = input;}}/>
+        <br />
+        <br />
+        <p>Medium:</p>
+        <input
+          id='medium'
+          placeholder='Medium'
+          ref={(textarea) => {_medium = textarea;}}/>
+        <br />
+        <br />
+        <p>Description:</p>
+        <textarea
+          id='description'
+          placeholder="Add Description Here....."
+          ref={(textarea) => {_description = textarea;}}/>
+        <p>Height:</p>
+        <input
+          type='text'
+          id='height'
+          placeholder='Height'
+          ref={(input) => {_height = input;}}/>
+        <p>Width:</p>
+        <input
+          type='text'
+          id='width'
+          placeholder='Width'
+          ref={(input) => {_width = input;}}/>
+        <p>Price:</p>
+        <input
+          type='text'
+          id='price'
+          placeholder='Price'
+          ref={(input) => {_price = input;}}/>
+        <br />
+        <br />
+        <button type='submit'>Add Work</button>
+      </form>
     </div>
   );
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,47 +5,26 @@ function ContactForm(){
     <div>
       <h4>Please fill out the form to contact the artist</h4>
       <form>
-        <p>Title:</p>
+        <p>Name:</p>
         <input
           type='text'
-          id='title'
-          placeholder='Title'
-          ref={(input) => {_author = input;}}/>
+          id='name'
+          placeholder='Your Name'
+          ref={(input) => {_name = input;}}/>
         <br />
-        <br />
-        <p>Medium:</p>
+        <p>Email:</p>
         <input
-          id='medium'
-          placeholder='Medium'
-          ref={(textarea) => {_content = textarea;}}/>
+          id='email'
+          placeholder='Email'
+          ref={(input) => {_email = textarea;}}/>
         <br />
-        <br />
-        <p>Description:</p>
+        <p>Inquiry:</p>
         <textarea
-          id='description'
-          placeholder="Add Description Here....."
-          ref={(textarea) => {_content = textarea;}}/>
-        <p>Height:</p>
-        <input
-          type='text'
-          id='height'
-          placeholder='Height'
-          ref={(input) => {_tags = input;}}/>
-        <p>Width:</p>
-        <input
-          type='text'
-          id='width'
-          placeholder='Width'
-          ref={(input) => {_tags = input;}}/>
-        <p>Price:</p>
-        <input
-          type='text'
-          id='price'
-          placeholder='Price'
-          ref={(input) => {_tags = input;}}/>
+          id='inquiry'
+          ref={(textarea) => {_inquiry = textarea;}}/>
         <br />
         <br />
-        <button type='submit'>Add Work</button>
+        <button type='submit'>Submit Request</button>
       </form>
     </div>
   );
