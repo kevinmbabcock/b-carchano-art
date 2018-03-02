a8669b6a7b651f7a2fe8f1d1977f242b9ecfbba0
Kevin M Babcock <kevinmbabcock@Kevins-MacBook-Air.local>
2018-03-02 15:19:45 -0800

    add form to Contact component

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,7 +3,50 @@ import React from 'react';
 function ContactForm(){
   return (
     <div>
-      <p>ContactForm component has loaded</p>
+      <h4>Please fill out the form to contact the artist</h4>
+      <form>
+        <p>Title:</p>
+        <input
+          type='text'
+          id='title'
+          placeholder='Title'
+          ref={(input) => {_author = input;}}/>
+        <br />
+        <br />
+        <p>Medium:</p>
+        <input
+          id='medium'
+          placeholder='Medium'
+          ref={(textarea) => {_content = textarea;}}/>
+        <br />
+        <br />
+        <p>Description:</p>
+        <textarea
+          id='description'
+          placeholder="Add Description Here....."
+          ref={(textarea) => {_content = textarea;}}/>
+        <p>Height:</p>
+        <input
+          type='text'
+          id='height'
+          placeholder='Height'
+          ref={(input) => {_tags = input;}}/>
+        <p>Width:</p>
+        <input
+          type='text'
+          id='width'
+          placeholder='Width'
+          ref={(input) => {_tags = input;}}/>
+        <p>Price:</p>
+        <input
+          type='text'
+          id='price'
+          placeholder='Price'
+          ref={(input) => {_tags = input;}}/>
+        <br />
+        <br />
+        <button type='submit'>Add Work</button>
+      </form>
     </div>
   );
 }
