039518f7470017624ebdae595451474b73854b05
Kevin M Babcock <kevinmbabcock@Kevins-MacBook-Air.local>
2018-03-08 10:30:45 -0800

    add logic for ADD_INQUIRY action. all tests passing

--- src/reducers/inquiriesListReducer.js
+++ src/reducers/inquiriesListReducer.js
@@ -3,10 +3,25 @@ const { initialState, types } = constants;
 
 export default (state = initialState.inquiriesList, action) => {
 
-  const { firstName, lastName, email, subject, message } = action;
+  const { firstName, lastName, email, subject, message, id } = action;
 
   switch (action.type) {
 
+    case types.ADD_INQUIRY: {
+      const newInquiryListEntry = Object.assign({}, state, {
+        [id] : {
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          subject: subject,
+          message: message,
+          newInquiry: true,
+          created: new Date().toString(),
+          id: id
+        }
+      });
+      return newInquiryListEntry;
+    }
 
 
     default:
