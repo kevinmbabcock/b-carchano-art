c5e8ba68b23557c1f7e98c6eaac0a22ea137363d
Kevin M Babcock <kevinmbabcock@Kevins-MacBook-Air.local>
2018-03-09 15:03:03 -0800

    update actions/index.js to contain all current actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -11,3 +11,36 @@ export const addWork = (title, medium, description, height, width, price ) => ({
   price,
   id: v4()
 });
+
+export const updateWork = (title, medium, description, height, width, price, featured, forSale, id) => ({
+  type: types.UPDATE_WORK,
+  title,
+  medium,
+  description,
+  height,
+  width,
+  price,
+  featured,
+  forSale,
+  id
+});
+
+export const deleteWork = (id) => ({
+  type: types.DELETE_WORK,
+  id
+});
+
+export const addInquiry = (firstName, lastName, email, subject, message) => ({
+  type: types.ADD_INQUIRY,
+  firstName,
+  lastName,
+  email,
+  subject,
+  message,
+  id: v4()
+});
+
+export const deleteInquiry = (id) ({
+  type: types.DELETE_INQUIRY,
+  id
+});
